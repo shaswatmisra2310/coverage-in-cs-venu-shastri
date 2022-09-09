28f9506554274af364bea7fae2cae6670c136763
shaswatmisra2310 <43674831+shaswatmisra2310@users.noreply.github.com>
2022-09-09 14:52:14 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -12,6 +12,8 @@ namespace TypewiseAlert.Test
         TypewiseAlert.BreachType.TOO_LOW);
       Assert.True(TypewiseAlert.inferBreach(34, 20, 30) ==
         TypewiseAlert.BreachType.TOO_HIGH);
+      TypewiseAlert.checkAndAlert(
+        TO_CONTROLLER, BatteryChar.CoolingType, 0);
     
     }
   }
